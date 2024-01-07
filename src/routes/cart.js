3c48a6d33fa53c211bde3009e645d96db7d2cd67
Đức thiện <ldthien203@gmail.com>
2024-01-07 16:04:02 +0700

    Create client view
    client view

---
+++ src/routes/cart.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+const CartController = require('../controllers/CartController');
+
+const CartRouter = Router();
+
+CartRouter.get('/cart', CartController.viewCart);
+CartRouter.get('/add-to-cart/:id', function(req, res, next){
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Product.findById(productId, function(err, product){
+        if(err){
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    });
+});
+
+module.exports = CartRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const AdminRouter = require("./admin");
 const AuthRouter = require("./auth");
 const ClientRouter = require("./client");
+const CartRouter = require("./cart")
 
 const routes = (app) => {
     app.use("/", AuthRouter)
     app.use("/admin", AdminRouter)
     app.use("/", ClientRouter)
+    app.use("/", CartRouter)
 }
 
 module.exports = routes;
