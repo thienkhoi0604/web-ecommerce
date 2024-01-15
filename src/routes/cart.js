6b49b1bff8213110ffcc5c66fd8c1e8c33071178
Đức thiện <ldthien203@gmail.com>
2024-01-15 17:35:35 +0700

    Update view
    View:
    Update cart, client, footer, header
    Add detail, products,

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -3,13 +3,16 @@ const CartController = require('../controllers/CartController');
 
 const CartRouter = Router();
 
-CartRouter.get('/cart', CartController.viewCart);
-CartRouter.get('/add-to-cart/:id', function(req, res, next){
+//CartRouter.get('/cart', CartController.viewCart);
+CartRouter.get('/cart', (req, res) => {
+    res.render('cart', { layout: false });
+});
+CartRouter.get('/add-to-cart/:id', function (req, res, next) {
     var productId = req.params.id;
     var cart = new Cart(req.session.cart ? req.session.cart : {});
 
-    Product.findById(productId, function(err, product){
-        if(err){
+    Product.findById(productId, function (err, product) {
+        if (err) {
             return res.redirect('/');
         }
         cart.add(product, product.id);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ const AdminRouter = require("./admin");
 const AuthRouter = require("./auth");
 const ClientRouter = require("./client");
 const CartRouter = require("./cart")
+const ProductRouter = require("./products")
+const DetailRouter = require("./detail")
 
 const routes = (app) => {
     app.use("/", AuthRouter)
     app.use("/admin", AdminRouter)
     app.use("/", ClientRouter)
     app.use("/", CartRouter)
+    app.use("/", ProductRouter)
+    app.use("/", DetailRouter)
 }
 
 module.exports = routes;
