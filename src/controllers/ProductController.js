463505bc07bfa0a3d20d3edd00d0e1714a79b78b
Đức thiện <ldthien203@gmail.com>
2024-01-20 14:38:34 +0700

    Update Product
    Update Product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,32 +1,9 @@
-const Product = require("../models/ProductModel");
+const Product = require('../models/ProductModel');
 
-const getAllProducts = async (req, res, next) => {
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch {
-        next(error);
+const ProductController = {
+    index(req, res, next) {
+        Product.find({
+            
+        })
     }
 }
-
-const getProductsByCategory = async (req, res, next) => {
-    const { category } = req.params;
-    try {
-        const products = await Product.find({ category });
-        res.json(products);
-    } catch {
-        next(error);
-    }
-}
-
-const searchProducts = async (req, res, next) => {
-    const { query } = req.query;
-    try {
-        const products = await Product.find({ name: { $regex: query, $option: "i" } });
-        res.json(products);
-    } catch {
-        next(error);
-    }
-}
-
-module.exports = { getAllProducts, getProductsByCategory, searchProducts };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
 const AdminRouter = require("./admin");
 const AuthRouter = require("./auth");
 const ClientRouter = require("./client");
-const CartRouter = require("./cart")
+const CartRouter = require("./cart");
+const ProductRouter = require("./products");
+const DetailRouter = require("./detail");
+const TestRouter = require("./test");
 
 const routes = (app) => {
     app.use("/", AuthRouter)
     app.use("/admin", AdminRouter)
     app.use("/", ClientRouter)
     app.use("/", CartRouter)
+    app.use("/", ProductRouter)
+    app.use("/", DetailRouter)
+    app.use("/", TestRouter)
 }
 
 module.exports = routes;
