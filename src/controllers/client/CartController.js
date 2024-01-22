d0e9812fd4f33c29dd062e1ce67146b10412e8c7
Đức thiện <ldthien203@gmail.com>
2024-01-22 19:53:37 +0700

    Update CartController
    Update Cart Controller (view cart)

--- src/controllers/client/CartController.js
+++ src/controllers/client/CartController.js
@@ -23,38 +23,6 @@ const CartController = {
             next(error);
         }
     },
-    async addToCart(req, res, next) {
-        try {
-            const productId = req.params._id;
-            const defaultUserId = 'defaultUserId';
-
-            const cart = await Cart.findOne({ createdBy: defaultUserId }).lean();
-            const product = await Product.findById(productId).lean();
-
-            if (!cart) {
-                const newCart = new Cart({
-                    userId: defaultUserId,
-                    products: [{ product: _id, quantity: 1 }],
-                    createdBy: defaultUserId,
-                })
-
-                await newCart.save();
-            } else {
-                const existingProduct = cart.products.find(p => p.product.toString() === productId)
-
-                if (existingProduct) {
-                    existingProduct.quantity += 1;
-                } else {
-                    cart.products.push({ product: productId, quantity: 1 });
-                }
-                await cart.save();
-            }
-
-            res.redirect('/cart');
-        } catch (error) {
-            next(error);
-        }
-    }
 }
 
 module.exports = CartController;
