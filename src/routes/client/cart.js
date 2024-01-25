e7db859c38bb64c86a43f0f37b3a58d08ab668f2
19120260 <thienkhoi0604@gmail.com>
2024-01-25 23:24:53 +0700

    handle add product in cart

--- src/routes/client/cart.js
+++ src/routes/client/cart.js
@@ -6,7 +6,9 @@ const CartRouter = Router();
 CartRouter.get('/', CartController.index);
 CartRouter.get('/all', CartController.getAllByUser);
 CartRouter.post('/', CartController.add);
+CartRouter.put('/quantity', CartController.updateQuantity);
 CartRouter.put('/quantity/add', CartController.updateQuantityAdd);
+CartRouter.put('/quantity/minus', CartController.updateQuantityMinus);
 CartRouter.delete('/', CartController.delete);
 
 module.exports = CartRouter;
